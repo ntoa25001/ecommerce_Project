d90129b57552df486e6d9b2151741cba825003c2
toannd <ninhductoan52@gmail.com>
2023-11-10 08:09:25 +0700

    toannd

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oad6b9dfgPlUL1qz5d7',
+        issuer: 'https://dev-03009535.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
